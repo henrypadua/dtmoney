4ab04f17ef522b5f0a4265b10cc075cbe5f95b15
henrypadua <henrybpj@live.com>
2021-06-03 00:29:50 -0300

    Criação dos estados

--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,43 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {createServer} from 'miragejs'
+import { createServer, Model } from "miragejs";
 import { App } from "./App";
 
 createServer({
-  routes() {
-    this.namespace = 'api';
+	models: {
+		transaction: Model,
+	},
 
-    this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Transaction 1',
-          amount: 400,
-          type: 'deposit',
-          category: 'Food',
-          createdAt: new Date()
-        }
-      ]
-    })
-  }
-})
+	seeds(server) {
+		server.db.loadData({
+			transactions: [
+				{
+					id: 1,
+					title: 'Freelancer Website',
+					value: 2500.00
+				}
+			]
+		})
+	},
+
+	routes() {
+		this.namespace = "api";
+
+		this.get("/transactions", () => {
+			return this.schema.all('transaction')
+		});
+
+		this.post("/transactions", (schema, request) => {
+			const data = JSON.parse(request.requestBody);
+
+			return schema.create('transaction', data);
+		});
+	},
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById("root")
 );
